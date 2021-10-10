731dcc00172f8e089f9fecd07da87f4485837553
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-10 19:06:01 +0200

    Added Tapis de Jeu

---
+++ frontend/src/views/Jeu.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="Jeu">
+  <tableau/>
+  <tapisJeu/>
+</div>
+</template>
+
+<script>
+import tableau from "../components/Tableau.vue"
+import tapisJeu from "../components/TapisJeu.vue"
+
+export default {
+    name: 'Jeu',
+  components: {
+    tableau,
+    tapisJeu
+  }
+}
+</script>
+
+<style>
+
+
+</style>
