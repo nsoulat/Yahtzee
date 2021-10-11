358da5bc2d1f4d486e44496d63b7ac97b43ae425
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-11 17:35:06 +0200

    Added a modal showing rules

--- frontend/src/views/Jeu.vue
+++ frontend/src/views/Jeu.vue
@@ -1,19 +1,35 @@
 <template>
 <div class="Jeu">
-  <tableau/>
-  <tapisJeu/>
+	<tableau/>
+	<tapisJeu/>
+
+	<modale :revele="revele" :toggleModale="toggleModale"></modale>
+	<div v-on:click="toggleModale" class="boutonRegles">?</div>
+
 </div>
 </template>
 
 <script>
 import tableau from "../components/Tableau.vue"
 import tapisJeu from "../components/TapisJeu.vue"
+import Modale from "../components/ModaleRegles.vue";
 
 export default {
     name: 'Jeu',
-  components: {
+	components: {
     tableau,
-    tapisJeu
+    tapisJeu,
+	modale: Modale
+  },
+  data() {
+    return {
+      revele: false
+    };
+  },
+  methods: {
+    toggleModale: function() {
+      this.revele = !this.revele;
+    }
   }
 }
 </script>
@@ -23,6 +39,18 @@ export default {
     margin-right: 1%;
     margin-top: 1%;
     margin-left: 1%
+}
+
+.boutonRegles {
+	position: absolute;
+	bottom: 2%;
+	height: 18px; 
+	width: 18px; 
+	background-color: rgb(255, 255, 255);
+	border: 3px solid rgba(0, 0, 0, 1);
+	border-radius: 50%; 
+	text-align: center;
+	font-weight: bold;
 }
 
 </style>
