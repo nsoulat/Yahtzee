66c2bbde87c649430a1c77cd8143698bbb7e2bdd
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-14 16:35:36 +0200

    Added Dice place component

---
+++ frontend/src/components/StockageDes.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="stockageDes">
+      
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.stockageDes {
+  width:400px;
+	height:80px;
+	background:rgb(0, 83, 44);
+  border: solid;
+  border-color: rgb(0, 31, 10);
+  border-radius: 4px;
+  border-width: 3px;
+}
+
+</style>
