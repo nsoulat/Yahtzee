9f2b7196db5fb513ce5f5480bb29ec5c3bd21d91
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-14 16:00:36 +0200

    Created component for game turn

---
+++ frontend/src/components/BoutonTourDeJeu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tourDeJeu">
+      <div class="rectangle">
+          Lancer les dés             <!-- Il faudra regarder si le joueur peut relancer et afficher autre chose selon le cas (ie le nombre de lancers restants) -->
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
