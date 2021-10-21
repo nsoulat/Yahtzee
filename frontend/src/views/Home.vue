6fc9423a74c42b09e5ea11b2649bc59b17e15742
Natalphée <nathanael.soulat@centrale-marseille.fr>
2021-10-21 21:42:31 +0200

    KD on several files

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,81 +1,80 @@
 <template>
-  <div class="home">
-    <h1>Yahtzee</h1>
-    <modale :afficheNomJoueurs="afficheNomJoueurs" :toggleModaleNomJoueurs="toggleModaleNomJoueurs"></modale>
-    <div v-on:click="toggleModaleNomJoueurs" class="rectangle" id="bouton2Joueurs">
-      2 Joueurs
+    <div class="home">
+        <h1>Yahtzee</h1>
+        <modale :afficheNomJoueurs="afficheNomJoueurs" :toggleModaleNomJoueurs="toggleModaleNomJoueurs"></modale>
+        <div v-on:click="toggleModaleNomJoueurs" class="rectangle" id="bouton2Joueurs">
+            2 Joueurs
+        </div>
+
+        <div id="espacement"></div>
+        <router-link class="rectangle" :to="{ name : 'Regles' }">
+            Règles
+        </router-link>
+
     </div>
-
-  <div id = "espacement"></div>
-  <router-link class="rectangle" :to="{ name : 'Regles' }">
-      Règles
-  </router-link>
-
-  </div>
 </template>
 
 <script>
-import modaleNomJoueurs from "../components/ModaleNomJoueurs.vue"
+    import modaleNomJoueurs from "../components/ModaleNomJoueurs.vue"
 
-export default {
-  components: {
-    modale: modaleNomJoueurs
-  },
-  data() {
-    return {
-      afficheNomJoueurs: false
-    };
-  },
-  methods: {
-  toggleModaleNomJoueurs: function() {
-    this.afficheNomJoueurs = !this.afficheNomJoueurs;
-  }
-}
-}
+    export default {
+        components: {
+            modale: modaleNomJoueurs
+        },
+        data() {
+            return {
+                afficheNomJoueurs: false
+            };
+        },
+        methods: {
+            toggleModaleNomJoueurs: function () {
+                this.afficheNomJoueurs = !this.afficheNomJoueurs;
+            }
+        }
+    }
 </script>
 
 <style>
 
-h1 {
-  font-size: 157px;
-  font-weight: bold;
-  text-align: center;
-  margin-bottom: 73px;
-  margin-top: 73px;
-}
+    h1 {
+        font-size: 157px;
+        font-weight: bold;
+        text-align: center;
+        margin-bottom: 73px;
+        margin-top: 73px;
+    }
 
-.rectangle {
-  width: 230px;
-  height: 110px;
-  background-color: white;
-  border: solid;
-  border-color:black;
-  border-width: 11px;
-  /* display: block; */
-  margin-left: auto;
-  margin-right: auto;
-  display : flex;
-  align-items : center;
-  justify-content: center;
-}
+    .rectangle {
+        width: 230px;
+        height: 110px;
+        background-color: white;
+        border: solid;
+        border-color: black;
+        border-width: 11px;
+        /* display: block; */
+        margin-left: auto;
+        margin-right: auto;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 
-a {
-  font-size: 30px;
-  color: #000000;
-  text-decoration: none;
-}
+    a {
+        font-size: 30px;
+        color: #000000;
+        text-decoration: none;
+    }
 
-a.router-link-exact-active {
-  color: #42b983;
-}
+        a.router-link-exact-active {
+            color: #42b983;
+        }
 
-#espacement {
-  height: 20px
-}
+    #espacement {
+        height: 20px
+    }
 
-#bouton2Joueurs {
-    font-size: 30px;
-    cursor: pointer;
-}
-
+    #bouton2Joueurs {
+        font-size: 30px;
+        cursor: pointer;
+    }
 </style>
