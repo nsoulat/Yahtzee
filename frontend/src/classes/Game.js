12aacc81155f8d7f804152a503e5bdf072ae568b
Natalphée <nathanael.soulat@centrale-marseille.fr>
2021-10-22 11:38:27 +0200

    new methods for game

--- frontend/src/classes/Game.js
+++ frontend/src/classes/Game.js
@@ -25,13 +25,50 @@ export default class Game {
             this.Figures.push(figure);
         });
 
-        this.CurrentTurn = this.Joueurs[0]; // initial player
+        this.CurrentPlayer = this.Joueurs[0]; // initial player
         this.TurnCounter = 0;
         console.log("Création d'un nouveau Jeu");
     }
 
     addPlayer(id, name) {
         this.Joueurs.push(new Joueur(id, name));
+    }
+
+    updateTotal() {
+        // TODO
+    }
+
+    endTurn() {
+        this.updateTotal();
+        this.CurrentPlayer = this.CurrentPlayer[++this.TurnCounter]
+        if (isTheEnd()) {
+            this.IsProgress = false;
+        }
+    }
+
+    isTheEnd() {
+        this.Figures.forEach(figure => {
+            if (figure.isClickable(this.CurrentPlayer, this.CurrentPlayer)) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    getWinners() {
+        let winners = [];
+        let maxPoints = 0;
+        this.Joueurs.forEach(joueur => {
+            let total = this.Figures.at(-1); // A améliorer ?
+            if (total > maxPoints) {
+                maxPoints = total;
+                winners = [joueur];
+            }
+            else if (total == maxPoints) {
+                winners.push(joueur);
+            }
+        });
+        return winners;
     }
 
 }
