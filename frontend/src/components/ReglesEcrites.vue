b63d73ed12107562b34de32f1b651a29945dfae0
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-25 12:09:08 +0200

    Added practical rules

--- frontend/src/components/ReglesEcrites.vue
+++ frontend/src/components/ReglesEcrites.vue
@@ -6,10 +6,10 @@
         <p>Vous avez le droit à 3 lancers de dés à chaque tour. Après chaque lancer, vous pouvez garder certains dés et donc ne relancer que ceux que vous souhaitez. Vous n’êtes pas obligés de faire les 3 lancers.</p>
         <h2>     Le marquage sur la grille :</h2>
         <p>
-            Quand vous êtes satisfaits de vos lancers, ou si vous avez déjà effectué les 3 possibles, il faut choisir une des 13 cases disponibles sur la grille, dans n’importe quel ordre. Cependant, à chaque fin de tour, vous êtes obligés d’en remplir une.<br/>
-            Chaque case correspond à un marquage de point différent. <br/>
-            La partie supérieure est le total, pour chaque valeur de 1 à 6, des faces de dès correspondant à cette valeur.<br/>
-            Si vous totalisez plus de 62 points pour la partie supérieure, vous obtenez un bonus de 35 points.<br/>
+            Quand vous êtes satisfaits de vos lancers, ou si vous avez déjà effectué les 3 possibles, il faut choisir une des 13 cases disponibles sur la grille, dans n’importe quel ordre. Cependant, à chaque fin de tour, vous êtes obligés d’en remplir une.<br>
+            Chaque case correspond à un marquage de point différent. <br>
+            La partie supérieure est le total, pour chaque valeur de 1 à 6, des faces de dès correspondant à cette valeur.<br>
+            Si vous totalisez plus de 62 points pour la partie supérieure, vous obtenez un bonus de 35 points.<br>
             La partie inférieure comporte différentes figures :
             <ul>
             <li>Brelan : Trois dés indiquant le même chiffre. Il vaut la somme des cinq dés.</li>
@@ -22,6 +22,14 @@
             </ul>
 
             Toutefois, étant obligé de remplir une case à chaque tour, il se peut que vous soyez obligés d’inscrire un 0 sur une case.
+        </p>
+        <h2>     Comment jouer ?</h2>
+        <p>
+            Après le premier lancer de dés automatique, vous avez la possibilité de cliquer sur les dés que vous ne souhaitez pas relancer directement sur le tapis de jeu.<br>
+            De même, si vous avez mis un dé de côté, vous pouvez cliquer dessus pour le remettre sur le tapis de jeu afin de le relancer.<br>
+            Attention, seuls les dés sur le tapis de jeu sont relancés quand vous cliquez sur "Relancer les dés".<br>
+            Lorsque vous êtes satisfaits des valeurs de vos dés, ou que vous n'avez plus la possibilité de relancer les dés, vous devez inscrire un score dans une des cases du tableau. <br>
+            Pour ce faire, vous avez simplement à cliquer sur la case de votre choix.
         </p>
   </div>
 </template>
