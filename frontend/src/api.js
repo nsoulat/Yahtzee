5c0c219c1109aaee1cfe99d4b7ebe531add0bdce
Mickaël Rolland <rolland.mick@orange.fr>
2021-10-25 22:36:02 +0200

    Hotfix #2

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
+const serverConfig = require("../../backend/config/config.json").SERVER;
+const HOSTNAME = serverConfig.HOST;
+const PORT = serverConfig.PORT;
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: `http://${HOSTNAME}:${PORT}/api`,
   headers: {
     "Content-type": "application/json"
   }
