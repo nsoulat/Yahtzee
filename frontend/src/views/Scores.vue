080a796cc867839a244d32dace41010b4a43d89a
Natalphée <nathanael.soulat@centrale-marseille.fr>
2021-10-26 16:05:27 +0200

    KD and remove unused methods

--- frontend/src/views/Scores.vue
+++ frontend/src/views/Scores.vue
@@ -1,62 +1,49 @@
 <template>
-	<div>
-		<h1>Meilleurs scores</h1>
-		<ul>
-			<li v-for="(score, index) in scores"
-				:key="index">
-				{{ score.name }} : {{ score.points }}
-			</li>
-		</ul>
-	</div>
+    <div>
+        <h1>Meilleurs scores</h1>
+        <ul>
+            <li v-for="(score, index) in scores"
+                :key="index">
+                {{ score.name }} : {{ score.points }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import ScoreDataService from "../services/ScoreDataService.js";
+    import ScoreDataService from "../services/ScoreDataService.js";
 
-export default {
-	name: 'Scores',
+    export default {
+        name: 'Scores',
 
-	data() {
-		return {
-			scores: [],
-			currentScore: null,
-			currentIndex: -1,
-			title: ""
-		};
-	},
-	methods: {
-		retrieveScores() {
-			ScoreDataService.getAll()
-			.then(response => {
-				this.scores = response.data;
-				console.log(response.data);
-			})
-			.catch(e => {
-				console.log(e);
-			});
-		},
-
-		removeAllScores() {
-			ScoreDataService.deleteAll()
-			.then(response => {
-				console.log(response.data);
-				this.refreshList();
-			})
-			.catch(e => {
-				console.log(e);
-			});
-		},
-	},
-		mounted() {
-			this.retrieveScores();
-		}
-};
+        data() {
+            return {
+                scores: [],
+                currentScore: null,
+                currentIndex: -1,
+                title: ""
+            };
+        },
+        mounted() {
+            this.retrieveScores();
+        },
+        methods: {
+            retrieveScores() {
+                ScoreDataService.getAll()
+                    .then(response => {
+                        this.scores = response.data;
+                    })
+                    .catch(e => {
+                        console.log(e);
+                    });
+            }
+        }
+    };
 </script>
 
 <style scoped>
 
-h1 {
-    font-size:88px;
-}
-
+    h1 {
+        font-size: 88px;
+    }
 </style>
