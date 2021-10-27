3415a38c19dd8a92b566341e5c014eafa3ddc9c1
Natalphée <nathanael.soulat@centrale-marseille.fr>
2021-10-27 09:42:17 +0200

    Complete vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,19 @@
+var config = null;
+try {
+    config = require("../backend/config/config.json");
+} catch (ex) {
+    console.log(`WARN: environnement instable -- config.json non trouvé`);
+    config = require("../backend/config/config.model.json");
+}
 module.exports = {
-  publicPath: ''
+    publicPath: '',
+    "devServer": {
+        "port": 8081, //config.SERVER.PORT,
+        "proxy": {
+            "/api": {
+                "target": `http://${config.SERVER.HOST}:${config.SERVER.HOST}`,
+                "changeOrigin": true
+            }
+        }
+    }
 }
